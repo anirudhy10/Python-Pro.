e54b6d5daf07e8517f03788cf9afe9b95cd94673
anirudhy10 <76209659+anirudhy10@users.noreply.github.com>
2022-06-25 21:01:18 +0530

    Update XOgame.py

--- XOgame/XOgame.py
+++ XOgame/XOgame.py
@@ -1,3 +1,4 @@
+# variables containg position of maze
 xstate=[0,0,0,0,0,0,0,0,0]
 zstate=[0,0,0,0,0,0,0,0,0]
 turn =1
@@ -5,9 +6,9 @@ turn =1
 def sum(a,b,c):
     return a+b+c
 
-
+# function printing game board
 def printBoard(xstate,zstate):
-    zero= 'X ' if xstate[0] else  ('O ' if zstate[0] else '0 ')
+    zero= 'X ' if xstate[0] else  ('O ' if zstate[0] else '0 ') # var for each position containing notation 
     one= 'X ' if xstate[1] else('O '  if zstate[1] else '1 ')
     two= 'X ' if xstate[2] else ('O ' if zstate[2] else '2 ')
     three= 'X ' if xstate[3] else ('O ' if zstate[3] else '3 ')
@@ -17,6 +18,7 @@ def printBoard(xstate,zstate):
     seven= 'X ' if xstate[7] else ('O ' if zstate[7] else '7 ')
     eight= 'X ' if xstate[8] else ('O ' if zstate[8] else '8 ')
     
+    #maze print
     print(f"{zero}|{one}|{two}")
     print(f"--|--|--")
     print(f"{three}|{four}|{five}")
@@ -24,6 +26,7 @@ def printBoard(xstate,zstate):
     print(f"{six}|{seven}|{eight}")
 
 
+    #checking game winning conditions
 def Wincheck(xstate,zstate):
     xwin=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     for win in xwin:
@@ -35,7 +38,7 @@ def Wincheck(xstate,zstate):
             return 0
     return -1
 
-
+#main body of the game
 print("Welcome to tic tac toe")
 while(True):
     printBoard(xstate,zstate)
